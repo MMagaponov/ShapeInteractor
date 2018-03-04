8952d43a023bdae18505cb6e860be98f56c72bdc
wolfchkov <wolfchkovaj@gmail.com>
2018-03-04 22:30:09 +0200

    Update AbstractShape.java
    Замечания

--- src/com/playtech/agaponov/shapes/AbstractShape.java
+++ src/com/playtech/agaponov/shapes/AbstractShape.java
@@ -4,6 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class AbstractShape {
+    /**
+    Параметры конкретной фигуры должны определятся в ее реализации 
+    как поля класса.
+    Map использовать не нужно, тем более мы ее не проходили пока еще.
+    **/
     private Map<String, Double> params = new HashMap<>();
 
     public AbstractShape(Map<String, Double> params) {
@@ -17,10 +22,16 @@ public abstract class AbstractShape {
 
     public abstract Double getSquare();
 
+    /**
+    Класс фигура не знает ни о каких параметрах 
+    **/
     public Map<String, Double> getParams() {
         return params;
     }
-
+    
+    /**
+    Класс фигура не знает ни о каких параметрах 
+    **/
     public void setParams(Map<String, Double> params) {
         this.params = params;
     }
